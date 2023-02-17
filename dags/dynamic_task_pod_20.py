e3e76ddce5e1fcbf55227ec5d59306d75a933c7b
Chris Bailey <chris.bailey@sas.com>
2023-02-17 11:27:44 -0500

    initial push for complete install and execute script

---
+++ dags/dynamic_task_pod_20.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+with DAG(dag_id="dynamic_task_pod_20", start_date=datetime.utcnow()) as dag:
+
+    @task
+    def add_one(x: int):
+        return x+1
+
+    @task
+    def sum_it(values):
+        total = sum(values)
+        print(f"Total was {total}")
+        return total
+
+    mylist = [i+1 for i in range(20)]
+    added_values = add_one.expand(x=mylist)
+    sum_it(added_values)
